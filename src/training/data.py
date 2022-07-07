e8a53ead16abde046ccd5ba843c6c3e912d0eba0
lukewys <wuyusongwys@gmail.com>
2022-07-07 09:53:34 -0400

    Debug summit multi GPUs

--- src/training/data.py
+++ src/training/data.py
@@ -496,6 +496,7 @@ def get_wds_dataset(
     else:
         pipeline.extend(
             [
+                wds.split_by_node,
                 wds.split_by_worker,
                 # at this point, we have an iterator over the shards assigned to each worker
                 wds.tarfile_to_samples(handler=log_and_continue),
