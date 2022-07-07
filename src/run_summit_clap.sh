eabe08f0d3e8a53514b6db3ff4b73fc2e8b5ed45
lukewys <wuyusongwys@gmail.com>
2022-07-07 18:25:57 -0400

    Debug summit multi GPUs

--- src/run_summit_clap.sh
+++ src/run_summit_clap.sh
@@ -33,9 +33,7 @@ python -m training.main \
     --datasetpath /gpfs/alpine/scratch/wuyusong/csc499/clap_data/webdataset_tar \
     --logs /gpfs/alpine/scratch/wuyusong/csc499/clap_data/audio_clip_logs \
     --openai-model-cache-dir /gpfs/alpine/scratch/wuyusong/csc499/clap_data/.cache/clip \
-    --horovod \
-    --local-loss \
-    --gather-with-grad
+    --horovod
 
 #2>&1 > $LOG
 #    --report-to "wandb" \
