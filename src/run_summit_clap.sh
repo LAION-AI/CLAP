e278eea44aaa3cf061152f52c361b7b3fe915f53
lukewys <wuyusongwys@gmail.com>
2022-07-07 10:09:01 -0400

    Debug summit multi GPUs

--- src/run_summit_clap.sh
+++ src/run_summit_clap.sh
@@ -18,7 +18,7 @@ python -m training.main \
     --lr=1e-3 \
     --wd=0.1 \
     --epochs=400 \
-    --workers=1 \
+    --workers=0 \
     --use-bn-sync \
     --freeze-text \
     --model HTSAT-tiny \
