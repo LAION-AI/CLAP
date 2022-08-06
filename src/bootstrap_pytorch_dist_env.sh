640c8cdcb4e20d070f6f4f0d3eb489a6662cd63d
lukewys <wuyusongwys@gmail.com>
2022-08-06 16:16:22 -0400

    Debug summit multi GPUs

--- src/bootstrap_pytorch_dist_env.sh
+++ src/bootstrap_pytorch_dist_env.sh
@@ -9,16 +9,6 @@ export WORLD_SIZE=$OMPI_COMM_WORLD_SIZE
 export MASTER_ADDR=$head
 export MASTER_PORT=29500 # default from torch launcher
 
-export NUMBA_CACHE_DIR=/tmp/
-
-export OMP_NUM_THREADS=7
-
-export HOROVOD_CACHE_CAPACITY=0
-
-wandb offline
-export WANDB_DIR=/gpfs/alpine/scratch/wuyusong/csc499/clap_data/audio_clip_logs_wandb
-export WANDB_CONFIG_DIR=/gpfs/alpine/scratch/wuyusong/csc499/clap_data/audio_clip_logs_wandb
-
 echo "Setting env_var RANK=${RANK}"
 echo "Setting env_var LOCAL_RANK=${LOCAL_RANK}"
 echo "Setting env_var WORLD_SIZE=${WORLD_SIZE}"
