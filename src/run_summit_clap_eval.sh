be55f0b9e4f9a29a300ba434b72c86795dd3c638
lukewys <wuyusongwys@gmail.com>
2022-09-02 13:44:35 -0400

    Bug fix in eval retrieval

--- src/run_summit_clap_eval.sh
+++ src/run_summit_clap_eval.sh
@@ -22,4 +22,4 @@ python -m evaluate.eval_retrieval_main \
     --batch-size=96 \
     --workers=4 \
     --openai-model-cache-dir /gpfs/alpine/scratch/wuyusong/csc499/clap_data/.cache/clip \
-    --resume /gpfs/alpine/scratch/wuyusong/csc499/clap_data/audio_clip_logs/2022_08_27-13_45_45-model_HTSAT-tiny-lr_0.001-b_96-j_4-p_fp32/checkpoints/epoch_latest.pt
+    --resume /gpfs/alpine/scratch/wuyusong/csc499/clap_data/audio_clip_logs/2022_08_28-17_16_16-model_HTSAT-tiny-lr_0.001-b_96-j_4-p_fp32_fewer_checkpoints/checkpoints/epoch_latest.pt
