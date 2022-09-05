59b32f78ec9b0e7c9dc4f26b48df0e8deece32f6
lukewys <wuyusongwys@gmail.com>
2022-09-05 12:23:21 -0400

    bug fix

--- src/open_clip/utils.py
+++ src/open_clip/utils.py
@@ -337,6 +337,6 @@ def load_class_label(path):
     if out is None:
         return None
     else:
-        key = Array(c_wchar, '\n'.join(list(out.keys())))
-        val = Array('i', out.values())
+        key = Array(c_wchar, '\n'.join(list(out.keys())), lock=False)
+        val = Array('i', out.values(), lock=False)
         return (key, val)
