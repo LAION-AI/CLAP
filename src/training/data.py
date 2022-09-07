2cd2963290ae7df0ef476702d3258b8c75b7d1b0
tianyu.zhang <zhangtianyupro@gmail.com>
2022-09-07 00:58:05 -0400

    debugging

--- src/training/data.py
+++ src/training/data.py
@@ -476,6 +476,7 @@ def collate_fn(batch):
                 for i in range(len(batch)):
                     tmp.append(batch[i][k][kk])
                 batch_dict[k][kk] = torch.vstack(tmp)
+            print("batch_dict", batch_dict)
         if isinstance(batch[0][k], torch.Tensor):
             batch_dict[k] = torch.stack([sample[k] for sample in batch])
         elif isinstance(batch[0][k], np.ndarray):
