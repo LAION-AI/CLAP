37f6d30a3e75e922e743a28183233062e14343a3
lukewys <wuyusongwys@gmail.com>
2022-09-30 15:13:22 -0400

    debug text freeze

--- src/training/train.py
+++ src/training/train.py
@@ -78,8 +78,20 @@ def train_one_epoch(
     end = time.time()
 
     for i, batch in enumerate(dataloader):
-        # logging.info(f"batch {i} of {num_batches_per_epoch}")
-        # logging.info(model.state_dict()['module.text_branch.resblocks.4.attn.in_proj_bias'][:10]) # For debug
+        logging.info(f"batch {i} of {num_batches_per_epoch}")
+        logging.info(model.state_dict()['module.text_branch.resblocks.4.attn.in_proj_bias'][:10]) # For debug
+        named_parameters = list(model.named_parameters())
+
+        # freeze text encoder
+        text_freeze_parameters = [
+            p
+            for n, p in named_parameters
+            if n.startswith("text_branch")
+               or n in ["positional_embedding", "text_projection"]
+               or n.startswith("token_embedding")
+               or n.startswith("ln_final")
+        ]
+        logging.info('Requires Grad:', text_freeze_parameters[0].requires_grad)
         step = num_batches_per_epoch * epoch + i
         if isinstance(scheduler, dict):
             for s in scheduler.values():
