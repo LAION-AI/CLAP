9da3bd7d9b2db7e040202e23e421729fe5027db2
lukewys <wuyusongwys@gmail.com>
2022-10-03 21:50:24 -0400

    Dataloader debug

--- src/training/data.py
+++ src/training/data.py
@@ -508,7 +508,15 @@ def preprocess(
     # TODO: (yusong) dataloader debug
     if torchaudio is not None:
         sample['waveform_16000'] = torchaudio.transforms.Resample(orig_sr, 16000)(audio_data)
-        sample['mel'] = torchaudio.transforms.MelSpectrogram()(sample['waveform_16000'])
+        sample['mel'] = torchaudio.transforms.MelSpectrogram(
+            sample_rate=48000,
+            n_fft=window_size,
+            win_length=window_size,
+            hop_length=480,
+            n_mels=64,
+            f_min=50,
+            f_max=14000
+        )(audio_data)
 
     try:
         json_dict_raw = json.loads(sample[text_ext].decode("utf-8"))
