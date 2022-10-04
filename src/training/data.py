a5b7bc6e0ee57993aa00a16207817e943bd70e05
lukewys <wuyusongwys@gmail.com>
2022-10-03 21:32:38 -0400

    Dataloader debug

--- src/training/data.py
+++ src/training/data.py
@@ -92,6 +92,31 @@ elif args.tmodel == "bart":
 _AUDIOSET_MAP_PATH = os.path.join(Path(__file__).parent, "audioset_textmap.npy")
 _AUDIOSET_MAP = np.load(_AUDIOSET_MAP_PATH, allow_pickle=True)
 
+# TODO: (yusong) dataloader debug
+from torchlibrosa.stft import Spectrogram, LogmelFilterBank
+window = 'hann'
+center = True
+pad_mode = 'reflect'
+ref = 1.0
+amin = 1e-10
+top_db = None
+window_size=1024
+hop_size=480
+sample_rate=48000
+fmin=50
+fmax=14000
+mel_bins=64
+
+# Spectrogram extractor
+spectrogram_extractor = Spectrogram(n_fft=window_size, hop_length=hop_size,
+                                         win_length=window_size, window=window, center=center, pad_mode=pad_mode,
+                                         freeze_parameters=True)
+
+# Logmel feature extractor
+logmel_extractor = LogmelFilterBank(sr=48000, n_fft=window_size,
+                                         n_mels=mel_bins, fmin=fmin, fmax=fmax, ref=ref, amin=amin, top_db=top_db,
+                                         freeze_parameters=True)
+
 
 def int16_to_float32(x):
     return (x / 32767.0).astype(np.float32)
@@ -483,6 +508,7 @@ def preprocess(
     # TODO: (yusong) dataloader debug
     if torchaudio is not None:
         sample['waveform_16000'] = torchaudio.transforms.Resample(orig_sr, 16000)(audio_data)
+        sample['mel'] = spectrogram_extractor(audio_data)
 
     try:
         json_dict_raw = json.loads(sample[text_ext].decode("utf-8"))
