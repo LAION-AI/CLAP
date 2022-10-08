5857891f231b99ef29edcb8525a63ee33a436335
lukewys <wuyusongwys@gmail.com>
2022-10-08 14:33:04 -0400

    Update evaluation for clotho and auiocaps

--- src/training/train.py
+++ src/training/train.py
@@ -586,6 +586,7 @@ def evaluate_clotho_audiocaps(
         for i, batch in enumerate(dataloader):
             audios = batch  # contains mel_spec, wavform, and longer list
             texts = [tokenizer(t) for t in batch['full_text']]
+            print(texts)
             texts = {k: torch.cat([t[k] for t in texts]) for k in texts[0].keys()}
 
             text_shape = texts["input_ids"].shape
