3c4b219b2b0dc5a1d99ea8d77ca78ce2b264d6a8
lukewys <wuyusongwys@gmail.com>
2022-10-13 01:12:18 -0400

    bug fix

--- src/open_clip/pann_model.py
+++ src/open_clip/pann_model.py
@@ -243,7 +243,7 @@ class Cnn14(nn.Module):
                 # local processing
                 if len(longer_list_idx) > 0:
                     import logging # TODO: remove
-                    logging.info(f"in longer!!! {longer_list_idx}, {input['longer'].sum()}")  # TODO: remove
+                    #logging.info(f"in longer!!! {longer_list_idx}, {input['longer'].sum()}")  # TODO: remove
                     fusion_x_local = x[longer_list_idx,1:,:,:].clone().contiguous()
                     FB,FC,FT,FF = fusion_x_local.size()
                     fusion_x_local = fusion_x_local.view(FB * FC, FT, FF)
@@ -277,7 +277,7 @@ class Cnn14(nn.Module):
             global_x = self.conv_block1(global_x, pool_size=(2, 2), pool_type='avg')
             if len(longer_list_idx) > 0:
                 import logging  # TODO: remove
-                logging.info(f"in longer!!! {longer_list_idx}, {input['longer'].sum()}")  # TODO: remove
+                #logging.info(f"in longer!!! {longer_list_idx}, {input['longer'].sum()}")  # TODO: remove
                 local_x = x[longer_list_idx,1:,:,:].contiguous()
                 TH = global_x.size(-2)
                 # local processing
