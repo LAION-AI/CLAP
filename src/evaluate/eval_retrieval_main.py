8aa081306f011d6b5f40a39b6d9135d0223929f5
lukewys <wuyusongwys@gmail.com>
2022-10-12 21:41:39 -0400

    update zeroshot retrieval

--- src/evaluate/eval_retrieval_main.py
+++ src/evaluate/eval_retrieval_main.py
@@ -53,7 +53,13 @@ def evaluate_zeroshot(model, data, start_epoch, args, writer):
 
         # get text features
         all_texts = ["The sound of " + t for t in args.class_index_dict.keys()]
-        all_texts = tokenize(all_texts)
+        # (yusong): a hack, can make it better
+        if args.tmodel == "transformer":
+            from open_clip import tokenize
+            all_texts = tokenize(all_texts)
+        else:
+            from training.data import tokenizer
+            all_texts = tokenizer(all_texts)
         all_text_features = model(None, all_texts, device)
         all_text_features = F.normalize(all_text_features, dim=-1).detach().cpu()
 
