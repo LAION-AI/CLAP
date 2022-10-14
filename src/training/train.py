776242db5b37a019f5f74f81dec7d389818bf88a
lukewys <wuyusongwys@gmail.com>
2022-10-13 21:27:30 -0400

    top-metric selection

--- src/training/train.py
+++ src/training/train.py
@@ -740,6 +740,7 @@ def calculate_selection_performance_clotho_audiocaps(val_metrics_per_dataset):
     Calculate performance for Clotho+AudioCaps for model selection.
     """
     selection_performance_all = []
+    logging.info(val_metrics_per_dataset)
     for n in val_metrics_per_dataset.keys():
         selection_performance = val_metrics_per_dataset[n]["audio_to_text_mAP@10"] + \
                       val_metrics_per_dataset[n]["text_to_audio_mAP@10"]
