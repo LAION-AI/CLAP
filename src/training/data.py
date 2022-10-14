ce2b689c510a66f7eb069c40341da3f2a8bafddb
lukewys <wuyusongwys@gmail.com>
2022-10-14 18:02:57 -0400

    bug fix

--- src/training/data.py
+++ src/training/data.py
@@ -507,7 +507,7 @@ def preprocess(
                     # In this case, we just use the whole audio.
                     mel_fusion = torch.stack([mel, mel, mel, mel], dim=0)
                     sample["mel_fusion"] = mel_fusion
-                    longer = torch.tensor([True]) # TODO
+                    longer = torch.tensor([False])
                 else:
                     ranges = np.array_split(list(range(0, total_frames-chunk_frames+1)), 3)
                     # print('total_frames-chunk_frames:', total_frames-chunk_frames,
@@ -577,7 +577,7 @@ def preprocess(
                 mel = get_mel(audio_data, audio_cfg)
                 mel_fusion = torch.stack([mel, mel, mel, mel], dim=0)
                 sample["mel_fusion"] = mel_fusion
-            longer = torch.tensor([True]) # TODO
+            longer = torch.tensor([False])
 
     sample["longer"] = longer
 
