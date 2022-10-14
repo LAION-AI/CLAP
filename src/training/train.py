915d65057adba555343cfe9486d2e02477747d20
lukewys <wuyusongwys@gmail.com>
2022-10-13 21:38:08 -0400

    top-metric selection

--- src/training/train.py
+++ src/training/train.py
@@ -753,16 +753,20 @@ def select_top_metric_clotho_audiocaps(metrics, val_metrics_per_dataset):
     # metrics: dict, key: metric name, value: metric value
     if not any(['-top' in k for k in metrics.keys()]):
         selection_performance = calculate_selection_performance_clotho_audiocaps(val_metrics_per_dataset)
+        metric_update = {}
         for k in metrics.keys():
-            metrics[k+'-top'] = metrics[k]
+            metric_update[k+'-top'] = metrics[k]
+        metrics.update(metric_update)
         metrics['top-selection-performance'] = selection_performance
         metrics['top-selection-epoch'] = metrics['epoch']
     else:
         selection_performance_new = calculate_selection_performance_clotho_audiocaps(val_metrics_per_dataset)
         selection_performance_old = metrics['top-selection-performance']
         if selection_performance_new > selection_performance_old:
+            metric_update = {}
             for k in metrics.keys():
-                metrics[k+'-top'] = metrics[k]
+                metric_update[k + '-top'] = metrics[k]
+            metrics.update(metric_update)
             metrics['top-selection-performance'] = selection_performance_new
             metrics['top-selection-epoch'] = metrics['epoch']
     return metrics
