4c23eb31b90d58b0869a57574dd0ea40ce47e005
lukewys <wuyusongwys@gmail.com>
2023-01-28 10:59:06 -0500

    dataset test

--- src/test_data_loader.py
+++ src/test_data_loader.py
@@ -4,11 +4,16 @@ from training.params import parse_args
 import torch
 import os
 from tqdm import tqdm
+from training.distributed import is_master, init_distributed_device, world_info_from_env
 
 args = parse_args()
 # sanitize model name for filesystem / uri use, easier if we don't use / in name as a rule?
 args.amodel = args.amodel.replace("/", "-")
 device = torch.device('cpu')
+
+# discover initial world args early so we can log properly
+args.distributed = False
+args.local_rank, args.rank, args.world_size = world_info_from_env()
 
 model, model_cfg = create_model(
     args.amodel,
