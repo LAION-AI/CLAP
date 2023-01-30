9c4908594501c44fdccc92341ca35a2adccabe01
lukewys <wuyusongwys@gmail.com>
2023-01-30 13:28:24 -0500

    fix bug in data loader test

--- src/test_data_loader.py
+++ src/test_data_loader.py
@@ -5,6 +5,7 @@ import torch
 import os
 from tqdm import tqdm
 from training.distributed import is_master, init_distributed_device, world_info_from_env
+from open_clip.utils import dataset_split
 
 args = parse_args()
 # sanitize model name for filesystem / uri use, easier if we don't use / in name as a rule?
@@ -14,6 +15,15 @@ device = torch.device('cpu')
 # discover initial world args early so we can log properly
 args.distributed = False
 args.local_rank, args.rank, args.world_size = world_info_from_env()
+
+if args.remotedata and is_master(args):
+    for dataset_name in args.datasetnames:
+        for split in dataset_split[dataset_name]:
+            if not os.path.exists(f"./json_files/{dataset_name}/{split}"):
+                os.makedirs(f"./json_files/{dataset_name}/{split}")
+            os.system(
+                f"aws s3 cp s3://s-laion-audio/webdataset_tar/{dataset_name}/{split}/sizes.json ./json_files/{dataset_name}/{split}/sizes.json"
+            )
 
 model, model_cfg = create_model(
     args.amodel,
