1181d9a6ba4469d89376c538c4c9ac06f3d00b42
tianyu-z <zhangtianyu.pro@gmail.com>
2023-02-06 18:54:26 -0500

    update heads of scripts

--- experiment_scripts/zeroshot_esc50.sh
+++ experiment_scripts/zeroshot_esc50.sh
@@ -1,43 +1,25 @@
 #!/bin/bash
 #SBATCH --comment clap
-#SBATCH --partition=gpu
+#SBATCH --partition=g40423
 #SBATCH --job-name=mclap
-#SBATCH --nodes 1
-#SBATCH --ntasks-per-node 1
+#SBATCH --nodes 3
+#SBATCH --ntasks-per-node 8
 #SBATCH --cpus-per-gpu=6
-#SBATCH --gres=gpu:1
+#SBATCH --exclusive
 #SBATCH --output=%x_%j.out
-#SBATCH --exclude gpu-st-p4d-24xlarge-[23,30,31,108,115,134,135,183,185,186,187,188,275,277,290,374]
 
-
-module load intelmpi
-source /opt/intel/mpi/latest/env/vars.sh
-export LD_LIBRARY_PATH=/opt/aws-ofi-nccl/lib:/opt/amazon/efa/lib64:/usr/local/cuda-11.0/efa/lib:/usr/local/cuda-11.0/lib:/usr/local/cuda-11.0/lib64:/usr/local/cuda-11.0:/opt/nccl/build/lib:/opt/aws-ofi-nccl-install/lib:/opt/aws-ofi-nccl/lib:$LD_LIBRARY_PATH
+module load openmpi
+module load cuda/11.7
 export NCCL_PROTO=simple
-export PATH=/opt/amazon/efa/bin:$PATH
-export LD_PRELOAD="/opt/nccl/build/lib/libnccl.so"
-
 export FI_EFA_FORK_SAFE=1
 export FI_LOG_LEVEL=1
 export FI_EFA_USE_DEVICE_RDMA=1 # use for p4dn
-
-#export NCCL_ALGO=ring
 export NCCL_DEBUG=info
-#export NCCL_DEBUG_SUBSYS=INIT,ENV,GRAPH,COLL
-
-export PYTHONFAULTHANDLER=1
-
-export CUDA_LAUNCH_BLOCKING=0
 export OMPI_MCA_mtl_base_verbose=1
 export FI_EFA_ENABLE_SHM_TRANSFER=0
 export FI_PROVIDER=efa
 export FI_EFA_TX_MIN_CREDITS=64
 export NCCL_TREE_THRESHOLD=0
-
-
-#export NCCL_P2P_DISABLE=1
-#export NCCL_IBEXT_DISABLE=1
-#export NCCL_SOCKET_IFNAME="eth0,en,eth,em,bond"
 
 # sent to sub script
 export HOSTNAMES=`scontrol show hostnames "$SLURM_JOB_NODELIST"`
