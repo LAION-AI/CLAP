dcfc6a96ccd1258bcbf34a3ec407f0525686af21
lukewys <wuyusongwys@gmail.com>
2023-02-14 21:54:27 -0500

    fixing esc50 retrieval

--- experiment_scripts/zeroshot_esc50.sh
+++ experiment_scripts/zeroshot_esc50.sh
@@ -1,30 +1,18 @@
 # run from CLAP directory
 
 python -m evaluate.eval_zeroshot_classification \
-  --save-frequency 5 \
-  --save-top-performance 3 \
-  --save-most-recent \
   --dataset-type="webdataset" \
   --precision="fp32" \
-  --warmup 0 \
   --batch-size=512 \
-  --wd=0.0 \
-  --epochs=50 \
   --workers=6 \
-  --use-bn-sync \
-  --freeze-text \
   --amodel HTSAT-tiny \
   --tmodel roberta \
-  --report-to "wandb" \
-  --wandb-notes "zeroshot-classification-esc50" \
   --datasetnames "esc50" \
+  --datasetpath "../" \
   --datasetinfos "train" \
   --seed 3407 \
-  --remotedata \
-  --logs /fsx/clap_logs \
-  --gather-with-grad \
-  --openai-model-cache-dir /fsx/yusong/transformers_cache \
+  --logs ./logs \
   --data-filling "repeatpad" \
   --data-truncating "rand_trunc" \
   --class-label-path="../class_labels/ESC50_class_labels_indices_space.json" \
-  --pretrained="/fsx/clap_logs/2022_10_17-02_08_21-model_HTSAT-tiny-lr_0.0001-b_96-j_6-p_fp32/checkpoints"
+  --pretrained="../Model train on LAION-Audio-630K with fusion/checkpoints/epoch_top_0.pt"
