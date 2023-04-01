ebe563550c4280cb2c6cffbc2ad766182e328e30
RetroCirce <retrocirce@126.com>
2023-04-01 13:39:56 -0700

    update pip for classifer guidance training

--- src/laion_clap/hook.py
+++ src/laion_clap/hook.py
@@ -205,7 +205,7 @@ class CLAP_Module(torch.nn.Module):
             audio_embed = audio_embed.detach().cpu().numpy()
         return audio_embed
 
-    def get_text_embedding(self, x, tokenizer = None):
+    def get_text_embedding(self, x, tokenizer = None, use_tensor = False):
         """get text embeddings from texts
 
         Parameters
@@ -214,10 +214,11 @@ class CLAP_Module(torch.nn.Module):
             text list 
         tokenizer: func:
             the tokenizer function, if not provided (None), will use the default Roberta tokenizer.
-        
+        use_tensor: boolean:
+            if True, the output will be the tesnor, preserving the gradient (default: False).      
         Returns
         ----------
-        text_embed : numpy.darray (N,D):
+        text_embed : numpy.darray | torch.Tensor (N,D):
             text embeddings that extracted from texts
         """ 
         self.model.eval()
@@ -226,7 +227,8 @@ class CLAP_Module(torch.nn.Module):
         else:
             text_input = self.tokenizer(x)
         text_embed = self.model.get_text_embedding(text_input)
-        text_embed = text_embed.detach().cpu().numpy()
+        if not use_tensor:
+            text_embed = text_embed.detach().cpu().numpy()
         return text_embed
         
     
